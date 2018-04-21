1adca4ea2b10fd4b7e3f900ab67d52da3e0d7376
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-04-21 20:20:27 -0300

    Fix, use template within template

--- src/vue-formatter.js
+++ src/vue-formatter.js
@@ -1,5 +1,6 @@
 'use strict';
-const templateReg = /<(?:\/)?template[\s\S]*?(?:lang="\s*(.*)\s*")?\s*>/ig;
+// const templateReg = /<(?:\/)?template[\s\S]*?(?:lang="\s*(.*)\s*")?\s*>/ig;
+const templateReg = /(<template\s*?(?:lang=["']\s*pug\s*['"])?\s*>)|(<\/template>)(?=[\s]*<script>)/ig;
 const scriptReg = /<(?:\/)?script[\s\S]*?(?:lang="\s*(.*)\s*")?\s*>/ig;
 const styleReg = /<(?:\/)?style[\s\S]*?(?:lang="\s*(.*)\s*")?\s*(?:scoped)?\s*>/ig;
 
@@ -16,21 +17,24 @@ const beautify = require('js-beautify');
 
 function getCode(code, block, expReg) {
   let split = code.split(expReg, 4);
+  // Encuentra las etiquetas templete,script,style
   let match = code.match(expReg);
   if (!match) {
     return null;
   }
   if (!/src/.test(match)) {
     if (block === "template") {
-      if (!split[1]) {
-        return match[0] + '\n' + beautify.html(split[2], config) + '\n' + match[1];
+      split = code.split(expReg, 6);
+      // console.log('->',code.split(expReg,6),'<-');
+      if (split.length > 0) {
+        return match[0] + '\n' + beautify.html(split[3], config) + '\n' + match[1];
       }
     } else if (block === "style") {
-      if (split[1] === undefined || split[1] === 'scss' || split[1] === 'less') {
+      if (typeof split[1] === 'undefined' || split[1] === 'scss' || split[1] === 'less') {
         return match[0] + '\n' + beautify.css(split[2], config) + '\n' + match[1];
       }
     } else {
-      if (split[1] === undefined || split[1] === 'TypeScript') {
+      if (typeof split[1] === 'undefined' || split[1] === 'TypeScript') {
         return match[0] + '\n' + beautify(split[2], config) + '\n' + match[1];
       }
     }
