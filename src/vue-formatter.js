0b89a9d123ac83f0423f02380ded590da9b0f0b5
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-04-21 20:58:07 -0300

    New test

--- src/vue-formatter.js
+++ src/vue-formatter.js
@@ -16,7 +16,7 @@ const config = {
 const beautify = require('js-beautify');
 
 function getCode(code, block, expReg) {
-  let split = code.split(expReg, 4);
+  let split;
   // Encuentra las etiquetas templete,script,style
   let match = code.match(expReg);
   if (!match) {
@@ -25,15 +25,18 @@ function getCode(code, block, expReg) {
   if (!/src/.test(match)) {
     if (block === "template") {
       split = code.split(expReg, 6);
-      // console.log('->',code.split(expReg,6),'<-');
+      // console.log(split);
+      // console.log(beautify.html(code.split(expReg, 6)[3], config));
       if (split.length > 0) {
         return match[0] + '\n' + beautify.html(split[3], config) + '\n' + match[1];
       }
     } else if (block === "style") {
+      split = code.split(expReg, 4);
       if (typeof split[1] === 'undefined' || split[1] === 'scss' || split[1] === 'less') {
         return match[0] + '\n' + beautify.css(split[2], config) + '\n' + match[1];
       }
     } else {
+      split = code.split(expReg, 4);
       if (typeof split[1] === 'undefined' || split[1] === 'TypeScript') {
         return match[0] + '\n' + beautify(split[2], config) + '\n' + match[1];
       }
@@ -46,9 +49,9 @@ module.exports = function (text) {
   if (!text) {
     return;
   } else {
-    let html = getCode(text, 'template', templateReg) ;
+    let html = getCode(text, 'template', templateReg);
     let script = getCode(text, 'script', scriptReg);
     let style = getCode(text, 'style', styleReg);
-    return (html?html+ '\n':'' )+(script?'\n'+script+ '\n':'')+(style?'\n'+style+'\n':'');
+    return (html ? html + '\n' : '') + (script ? '\n' + script + '\n' : '') + (style ? '\n' + style + '\n' : '');
   }
 };
--- test/test-b.vue
+++ test/test-b.vue
@@ -1,4 +1,25 @@
-<template>undefined</template>
+<template>
+<v-ons-page :infinite-scroll="loadMore">
+  <v-ons-list class="bg-trans">
+
+    <pull-hook :on-action="onAction"></pull-hook>
+
+    <div style="text-align: center; margin: 40px; color: #666" v-if="isLoading">
+      <p>
+        <v-ons-progress-circular indeterminate></v-ons-progress-circular>
+      </p>
+    </div>
+
+    <template v-for="(item, $key) in list" v-if="!isLoading">
+<v-ons-list-title>Fecha {{ new Date($key) | morph-date('DD-MM-YYYY') }}</v-ons-list-title>
+<v-ons-list modifier="inset">
+<item-partido v-for="(partido, index) in list[$key]" :key="index" :partido="partido"></item-partido>
+</v-ons-list>
+</template>
+
+  </v-ons-list>
+</v-ons-page>
+</template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
